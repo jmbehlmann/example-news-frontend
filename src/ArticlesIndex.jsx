a11ff5c13ad3a31cc90abf5460b47b40da5ea91c
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-17 14:30:23 -0600

    search to backend

--- src/ArticlesIndex.jsx
+++ src/ArticlesIndex.jsx
@@ -1,31 +1,45 @@
 import axios from "axios"
-import { useState } from "react"
+import { useState, useId } from "react"
 
 export function ArticlesIndex() {
   const [articles, setArticles] = useState([])
-  const apiKey = import.meta.env.VITE_API_KEY
   const [searchTerm, setSearchTerm] = useState("")
+  // const apiKey = import.meta.env.VITE_API_KEY
+  const category = useId();
 
 
 
   const getArticles = () => {
-    console.log("getting articles")
-    axios.get(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${apiKey}`)
-    .then((response) => {
-      console.log(response.data)
-      setArticles(response.data.articles)
-    })
+    axios
+      .get(`http://localhost:3000/articles.json?q=${searchTerm}`)
+      .then((response) => {
+        console.log(response.data)
+        setArticles(response.data.articles)
+      })
   }
 
 
   return (
     <div>
       <h1>All Articles</h1>
+      <label htmlFor="category">Choose a category:</label>
+      <select name="category" id={category}>
+        <option value="business">Business</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="general">General</option>
+        <option value="health">Health</option>
+        <option value="science">Science</option>
+        <option value="sports">Sports</option>
+        <option value="technology">Technology</option>
+      </select>
       <p>Search: <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value) } list="titles" /></p>
       <button onClick={getArticles}>Get Articles</button>
       {articles.map((article) => (
-        <div key={article.url}>
+        <div key={article.id}>
           <p>Title: {article.title}</p>
+          <div>
+            <img width="300px" src={article.urlToImage} />
+          </div>
           <a href={article.url}>Go to article</a>
         </div>
       ))}
